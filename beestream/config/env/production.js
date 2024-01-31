c36a34e3ee470ad52475bbfa310b0e208a16ad32
williebees <66076410+williebees@users.noreply.github.com>
2024-01-31 14:36:47 -0500

    Changed mongodb adress to point to the new database location.

--- beestream/config/env/production.js
+++ beestream/config/env/production.js
@@ -1,14 +1,14 @@
 module.exports = {
   // Development Configuration Options
   //our database address
-  db : 'mongodb://localhost:27017/beeDB',
+  db : 'mongodb://mongo-mais.cs.appstate.edu:27017/beeDB',
   //Path to the directory containing hive folders
   videoPath: '/usr/local/bee/appmais',
   //Path to the ffmpeg installation
   // ffmpegPath: '/usr/local/apache2/htdocs/cs/bee/ffmpeg-3.4.2/',
   ffmpegPath: '/usr/bin/ffmpeg',
   //Port to run the server on
-  port: 8082,
+  port: 8084,
   //The avaliable datasets to view on the dashboard
   dataSets: {
     video: [
