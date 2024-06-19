9a5ccb9abdd21a12b5d58e4520fd3dfc16b3c1b9
campellcl <campellcl@appstate.edu>
2024-06-19 10:53:23 -0400

    Bind to port 0.0.0.0 for k8

--- beestream/server.js
+++ beestream/server.js
@@ -10,6 +10,6 @@ const app = require('./config/express')(db);
 //get the port to run on from the config file.
 const port = require('./config/config.js').port;
 
-app.listen(port);
+app.listen(port, '0.0.0.0');
 module.exports = app; //Export the running application.
 console.log(`Beestream running at http://localhost:${port}/ in ${process.env.NODE_ENV} mode.`); //TODO: edit to reflect the address of the server host.
